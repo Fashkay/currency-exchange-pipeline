b25da849f312e7d61c31130770d09db177469508
Fashkay <fashtravels2022@gmail.com>
2023-11-06 16:33:33 +0100

    updated

--- data_acquisition.py
+++ data_acquisition.py
@@ -1,23 +1,26 @@
 import requests
 
-# Define API URL and parameters
-base_url = "https://api.exchangeratesapi.io/v1/latest"
-api_key = "940a8603fffdd49ac7d242bc35b6f3bf"
-base_currency = "USD"
-target_currency = "EUR"
+def get_exchange_rate(base_currency, target_currency, api_key):
+    """
+    Get the exchange rate from the API.
 
-# Construct API request URL
-api_request_url = f"{base_url}?base={base_currency}&symbols={target_currency}"
-headers = {
-    "Authorization": f"Bearer {api_key}"
-}
+    Args:
+        base_currency (str): The base currency for conversion.
+        target_currency (str): The target currency for which the exchange rate is requested.
+        api_key (str): The API key for authentication.
 
-# Send API request and retrieve response
-response = requests.get(api_request_url, headers=headers)
+    Returns:
+        float: The exchange rate.
+    """
+    base_url = "https://api.exchangeratesapi.io/v1/latest"
+    api_request_url = f"{base_url}?base={base_currency}&symbols={target_currency}"
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
 
-if response.status_code == 200:
-    # Extract exchange rate from response JSON
-    exchange_rate = response.json()["rates"][target_currency]
-    print(f"The exchange rate from {base_currency} to {target_currency} is: {exchange_rate}")
-else:
-    print(f"Error: Unable to retrieve exchange rate. Status code: {response.status_code}")
+    response = requests.get(api_request_url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()["rates"][target_currency]
+    else:
+        raise Exception(f"Error: Unable to retrieve exchange rate. Status code: {response.status_code}")
